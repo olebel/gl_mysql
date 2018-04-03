59540c0598d580b19542bada2d0f96e101d8349d
olebel <obeletskyi@carbonite.com>
2018-04-03 04:36:51 +0300

    Added mysql user,group tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,11 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe group('mysql') do
+  it { should exist }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe user('mysql') do
+  it { should exist }
+  its('group') { should eq 'mysql' }
 end
